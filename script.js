5783bbdfcaf412b8b2c20c6f74965a1a07c6ad61
samsujjoha313 <samsujjoha313@gmail.com>
2022-01-04 20:42:25 +0600

    portfolio is added

---
+++ script.js
@@ -0,0 +1,303 @@
+// var numbers = [1,2,3,4,5];
+// var result = numbers.push(11,22,33);
+// document.write(result);
+
+// var numbers = [1,2,3,4,5,6];
+// var result = numbers.map((num)=>{
+//     return 2 * num;
+// });
+// document.write(result);
+
+// var numbers = [1,2,3,4,5,6];
+// var result = numbers.map((num)=>{
+//     return 2 * num;
+// });
+// document.write(result);
+
+// var numbers = [1,2,3,4,5,6];
+// var result = numbers.reduce((prevValue,currentValue,currentIndex,arr)=>{
+//     return prevValue + currentValue;
+// },0);
+// document.write(result);
+
+// let i = 0;
+// for(; i<=5; i++){
+//     document.write(i);
+// }
+
+// var java = "i love javacsript";
+// for (element of java){
+//     document.write(element);
+// }
+
+// var myObj = {
+//     name: "javascript",
+//     age: 25,
+//     city: "rangpur"
+// };
+// var keys = object.keys(myObj);
+// var values = object.values(myObj);
+// var entries = object.entries(myObj);
+// document.write(keys);
+// document.write(values);
+// document.write(entries);
+
+// function gh(x=2){
+//     return x;
+// }
+// document.write(gh(null));
+
+// function myFun(x=10){
+//     return x;
+// }
+// document.write(myFun(undefined));
+
+// var numbers = [2,3,4];
+// var result = [...numbers];
+// document.write(result);
+
+// var numbers1 = [1,2,3];
+// var numbers2 = [4,5,6];
+// var result = [...numbers1 , ...numbers2];
+// document.write(result);
+
+// function myFun(a,...params){
+//     document.write(params);
+// }
+// myFun(1,2,3,4,5);
+
+
+// function myFun(a,...params){
+//     document.write(a,params)
+// }
+// myFun(1,2,3,4);
+
+// const user = {
+//     id: 222,
+//     name: "samsujjoha",
+//     city: "rangpur"
+// };
+// const {city} = user;
+// document.write(city);
+
+
+// const user = {
+//     id: 111,
+//     name: "samsujjoha",
+//     city: "rangpur"
+// };
+// const{name:title} = user;
+// document.write(title);
+
+// const user = {
+//     id: 111,
+//     name: "joha",
+//     city: "rangpur",
+//     education:{
+//         degree: "masters",
+//     },
+// };
+// const {education :{degree:title}} = user;
+// document.write(title);
+
+
+// var numbers = [1,2,[55,100,400],3,4];
+// var [,,[,a,b],,] = numbers;
+// document.write(a,b);
+
+var a = 1;
+var b = 2;
+// var temp = a;
+// a = b;
+// b= temp;
+// document.write(a,b);
+
+// [b,a] = [a,b];
+// document.write(a,b);
+
+// import {pi,a} from './external/js';
+// document.write(pi,a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
